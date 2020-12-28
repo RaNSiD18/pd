3f4785734187b4fc0755cff5aaf09f7558939c76
Alexsey <46074417+RaNSiD18@users.noreply.github.com>
2020-12-28 21:46:14 +0300

    Add files via upload

---
+++ SaveInventor2019(ver 1.1)/SaveInventor2019(ver 1.1)/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace SaveInventor2019_ver_1._1_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button1.Image = global::SaveInventor2019_ver_1._1_.Properties.Resources._1;
+            this.button1.Location = new System.Drawing.Point(62, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save project";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buSaveModel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SaveInventor2019_ver_1._1_.Properties.Resources._02fs1c1vv00;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(252, 142);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SaveProjectInventor";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ SaveInventor2019(ver 1.1)/SaveInventor2019(ver 1.1)/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventor;
+using System.Windows.Forms;
+
+namespace SaveInventor2019_ver_1._1_
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// ThisApplication - Объект для определения активного состояния Инвентора
+        /// </summary>
+        private Inventor.Application ThisApplication = null;
+        /// <summary>
+        /// Словарь для хранения ссылок на документы деталей
+        /// </summary>
+        //private Dictionary<string, PartDocument> oPartDoc = new Dictionary<string, PartDocument>();
+        /// <summary>
+        /// Словарь для хранения ссылок на определения деталей
+        /// </summary>
+        //private Dictionary<string, PartComponentDefinition> oCompDef = new Dictionary<string, PartComponentDefinition>();
+        /// <summary>
+        /// Словарь для хранения ссылок на инструменты создания деталей
+        /// </summary>
+        //private Dictionary<string, TransientGeometry> oTransGeom = new Dictionary<string, TransientGeometry>();
+        /// <summary>
+        /// Словарь для хранения ссылок на транзакции редактирования
+        /// </summary>
+        //private Dictionary<string, Transaction> oTrans = new Dictionary<string, Transaction>();
+        /// <summary>
+        /// Словарь для хранения имен сохраненных документов деталей
+        /// </summary>
+        //private Dictionary<string, string> oFileName = new Dictionary<string, string>();
+
+        //private AssemblyDocument oAssemblyDocName;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+
+            try
+            {
+                //Проверка наличия активного состояния Инвентора.
+                ThisApplication = (Inventor.Application)System.Runtime.InteropServices.Marshal.GetActiveObject("Inventor.Application");
+                if (ThisApplication != null) MessageBox.Show("Инвентор открыт!", "SaveProjectInventor");
+            }
+            catch
+            {
+                // Если Инвентор не запущен, то возвращаемся в основную программу.
+                MessageBox.Show("Запустите Инвентор!", "SaveProjectInventor");
+            }
+        }
+
+        private void GetExtrudeFeature()
+        {
+            try
+            {
+                //Проверка наличия активного состояния Инвентора.
+                ThisApplication = (Inventor.Application)System.Runtime.InteropServices.Marshal.GetActiveObject("Inventor.Application");
+            }
+            catch
+            {
+                // Если Инвентор не открыт, то возвращаемся в основную программу.
+                System.Windows.Forms.MessageBox.Show("Запустите Инвентор!");
+                return;
+            }
+
+            String oName = "Test_save_project";
+
+            String dir = "C:\\Users\\serwa\\Desktop\\";
+
+            int i = 0;
+            if (i < 100)
+            {
+
+                /// 
+                //ThisApplication.ActiveDocument.SaveAs(dir + oName + "_" + i + ".ipt", true);
+                //MessageBox.Show("Файл " + oName + "_" + i + ".ipt" + " сохранился по пути \n" + dir);
+                ThisApplication.ActiveDocument.DisplayName = "Test_save_project_" + i;
+
+                saveFileDialog1.FileName = ThisApplication.ActiveDocument.DisplayName;
+                saveFileDialog1.Filter = "Inventor Part Document|*.*";
+                saveFileDialog1.Title = oName;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if (!string.IsNullOrWhiteSpace(saveFileDialog1.FileName))
+                    {
+                        ThisApplication.ActiveDocument.SaveAs(saveFileDialog1.FileName, false);
+                        MessageBox.Show("Файл " + ThisApplication.ActiveDocument.DisplayName + " сохранился по пути \n" + saveFileDialog1.FileName);
+                        i++;
+                    }
+                }
+            }
+        }
+
+        private void buSaveModel_Click(object sender, EventArgs e)
+        {
+            GetExtrudeFeature();
+        }
+    }
+}
