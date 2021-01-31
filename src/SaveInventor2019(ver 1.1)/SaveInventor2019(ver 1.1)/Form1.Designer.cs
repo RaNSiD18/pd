e646b9efe4a680e0d0f2066ac16ab8050b629e51
Alexsey <46074417+RaNSiD18@users.noreply.github.com>
2021-01-31 10:46:53 +0300

    Add files via upload

---
+++ src/SaveInventor2019(ver 1.1)/SaveInventor2019(ver 1.1)/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace SaveInventor2019_ver_1._1_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button1.Image = global::SaveInventor2019_ver_1._1_.Properties.Resources._1;
+            this.button1.Location = new System.Drawing.Point(55, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save project";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buSaveModel_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button2.Image = global::SaveInventor2019_ver_1._1_.Properties.Resources._1;
+            this.button2.Location = new System.Drawing.Point(196, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 57);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Convert project";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.buConvertModel_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.GhostWhite;
+            this.button3.Image = global::SaveInventor2019_ver_1._1_.Properties.Resources._1;
+            this.button3.Location = new System.Drawing.Point(55, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(276, 49);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Open project";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.buOpenModel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::SaveInventor2019_ver_1._1_.Properties.Resources._02fs1c1vv00;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(379, 180);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SaveProjectInventor";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ src/SaveInventor2019(ver 1.1)/SaveInventor2019(ver 1.1)/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventor;
+using System.Windows.Forms;
+using System.IO;
+using Application = System.Windows.Forms.Application;
+using File = System.IO.File;
+
+namespace SaveInventor2019_ver_1._1_
+{
+    public partial class Form1 : Form
+    {
+        private const string NAME_PROJECT = "SaveProjectInventor";
+
+        private Inventor.Application ThisApplication = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+        }
+
+        private void SaveProject()
+        {
+            try
+            {
+                //Проверка наличия активного состояния Инвентора.
+                ThisApplication = (Inventor.Application)System.Runtime.InteropServices.Marshal.GetActiveObject("Inventor.Application");
+            }
+            catch
+            {
+                // Если Инвентор не открыт, то возвращаемся в основную программу.
+                System.Windows.Forms.MessageBox.Show("Запустите Инвентор!");
+            }
+
+
+            if (ThisApplication.ActiveDocument.FileSaveCounter == 0)
+            {
+                MessageBox.Show("Открылось внутреннее окно сохранения Inventor");
+                // Первое сохранение детали
+                ThisApplication.ActiveDocument.Save();
+            }
+            else
+            {
+                saveFileDialog1.FileName = ThisApplication.ActiveDocument.DisplayName;
+                saveFileDialog1.Filter = "Inventor Part Document|*.ipt|Inventor Part Document|*.step|Inventor Part Document|*.stp";
+                saveFileDialog1.Title = NAME_PROJECT;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    if (!string.IsNullOrWhiteSpace(saveFileDialog1.FileName))
+                    {
+                        //ThisApplication.ActiveDocument.Save2(false, saveFileDialog1.FileName);
+                        ThisApplication.ActiveDocument.SaveAs(saveFileDialog1.FileName, false);
+                        MessageBox.Show("Файл " + ThisApplication.ActiveDocument.DisplayName + " сохранился по пути \n" + saveFileDialog1.FileName);
+                    }
+                }
+            }
+        }
+
+        private void buSaveModel_Click(object sender, EventArgs e)
+        {
+            SaveProject();
+        }
+
+        private void ConvertProject()
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Inventor Part Document|*.ipt";
+            openFileDialog1.Title = NAME_PROJECT;
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.Move(openFileDialog1.FileName, openFileDialog1.FileName.Replace(".ipt", ".step"));// переименование файла
+                MessageBox.Show("Файл " + openFileDialog1.FileName + " изменен на \n" + openFileDialog1.FileName.Replace(".ipt", ".step"));
+            }
+        }
+
+        private void buConvertModel_Click(object sender, EventArgs e)
+        {
+            ConvertProject();
+        }
+
+        private void OpenProject()
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.Filter = "Inventor Part Document|*.ipt|Inventor Part Document|*.step|Inventor Part Document|*.stp";
+            openFileDialog1.Title = NAME_PROJECT;
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var name = openFileDialog1.FileName;
+                System.Diagnostics.Process.Start(@name);
+            }
+        }
+
+        private void buOpenModel_Click(object sender, EventArgs e)
+        {
+            OpenProject();
+        }
+    }
+}
